561bb31d9c5ec308a0e1c16aa3480757d5a08ace
Cyril Rohr <cyril.rohr@gmail.com>
2015-11-03 15:40:16 +0000

    buildcurl

---
+++ bin/util/buildcurl.sh
@@ -0,0 +1,17 @@
+buildcurl() {
+	local cache_dir="$CACHE_DIR/buildcurl"
+	local binary=$(basename $1 .tar.gz)
+	local recipe=${binary/-*/}
+	local version=${binary/*-/}
+	local out_prefix="/app/.heroku/php"
+	local output=$(mktemp)
+	mkdir -p "$cache_dir"
+	if [ ! -f $cache_dir/$binary.tar.gz ]; then
+		echo "         Compiling $binary for the first time will take a few minutes..." 1>&2
+		curl -sSL --fail --get --retry 3 ${BUILDCURL_URL:="buildcurl.com"} \
+			-d recipe=$recipe -d version=$version -d target=$TARGET -d prefix=$out_prefix \
+			-o $output || ( cat $output 1>&2 && exit 1 )
+		mv $output $cache_dir/$binary.tar.gz
+	fi
+	cat $cache_dir/$binary.tar.gz
+}
