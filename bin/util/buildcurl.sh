134082c6cbb7e989371665f5efe1ff1a017611a4
Rijnhard Hessel <rijnhard@platforma.io>
2016-08-30 20:22:21 +0200

    added another funciton so that composer isnt pointlessly built

--- bin/util/buildcurl.sh
+++ bin/util/buildcurl.sh
@@ -1,3 +1,21 @@
+cachecurl() {
+	local cache_dir="$cache_dir/cachecurl"
+	local binary=$(basename $1 .tar.gz)
+	local recipe=${binary/-*/}
+	local version=${binary/*-/}
+	local output=$(mktemp)
+
+	echo "Cachecurl ($1) for binary:$binary in output:$output but to $cache_dir/$binary.tar.gz" 1>&2
+	mkdir -p "$cache_dir"
+	if [ ! -f $cache_dir/$binary.tar.gz ]; then
+		echo "         Downloading Binary for the first time" 1>&2
+		curl -sSL --get --retry 3 $1 \
+			-o $output && gunzip -t $output &>/dev/null || ( cat $output 1>&2 && exit 1 )
+		mv $output $cache_dir/$binary.tar.gz
+	fi
+	cat $cache_dir/$binary.tar.gz
+}
+
 buildcurl() {
 	local cache_dir="$cache_dir/buildcurl"
 	local binary=$(basename $1 .tar.gz)
