c675cf996658db5cca455d1db0ef1f31e7d32069
Rijnhard Hessel <rijnhard@teleforge.co.za>
2018-02-28 22:10:55 +0200

    target stable

--- bin/util/newrelic.sh
+++ bin/util/newrelic.sh
@@ -4,8 +4,8 @@ install_newrelic_ext() {
     # special treatment for New Relic; we enable it if we detect a license key for it
     # otherwise users would have to have it in their require section, which is annoying in development environments
     NEW_RELIC_LICENSE_KEY=${NEW_RELIC_LICENSE_KEY:-}
-    if [[ "$engine" == "php" && -n "$NEW_RELIC_LICENSE_KEY" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-newrelic 2>/dev/null; then
-        if $engine -n $(which composer) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-newrelic:*" >> $build_dir/.heroku/php/install.log 2>&1; then
+    if [[ "$engine" == "php" && -n "$NEW_RELIC_LICENSE_KEY" ]] && ! $engine -n $(which composer) show -d "$BUILD_DIR/.heroku/php" --installed --quiet heroku-sys/ext-newrelic 2>/dev/null; then
+        if $engine -n $(which composer) require --update-no-dev -d "$BUILD_DIR/.heroku/php" -- "heroku-sys/ext-newrelic:*" >> $BUILD_DIR/.heroku/php/install.log 2>&1; then
             echo "- New Relic detected, installed ext-newrelic" | indent
         else
             warning_inline "New Relic detected, but no suitable extension available"
@@ -20,6 +20,6 @@ install_newrelic_userini() {
 '${NEW_RELIC_CONFIG_FILE}'"
         fi
         notice_inline "Using custom New Relic config '${NEW_RELIC_CONFIG_FILE}'"
-        ( cd $build_dir/.heroku/php/etc/php/conf.d; ln -s "../../../../../${NEW_RELIC_CONFIG_FILE}" "ext-newrelic.user.ini" )
+        ( cd $BUILD_DIR/.heroku/php/etc/php/conf.d; ln -s "../../../../../${NEW_RELIC_CONFIG_FILE}" "ext-newrelic.user.ini" )
     fi
 }
