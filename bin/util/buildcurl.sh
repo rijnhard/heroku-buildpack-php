d5ac75527889e2027f703e963957c11f8df9617d
Rijnhard Hessel <rijnhard@teleforge.co.za>
2018-02-28 21:21:16 +0200

    figured out why the variables were normalized

--- bin/util/buildcurl.sh
+++ bin/util/buildcurl.sh
@@ -1,23 +1,23 @@
 cachecurl() {
-	local cache_dir="$cache_dir/cachecurl"
+	local CACHE_DIR="$CACHE_DIR/cachecurl"
 	local binary=$(basename $1 .tar.gz)
 	local recipe=${binary/-*/}
 	local version=${binary/*-/}
 	local output=$(mktemp)
 
-	echo "Cachecurl ($1) for binary:$binary in output:$output but to $cache_dir/$binary.tar.gz" 1>&2
-	mkdir -p "$cache_dir"
-	if [ ! -f $cache_dir/$binary.tar.gz ]; then
+	echo "Cachecurl ($1) for binary:$binary in output:$output but to $CACHE_DIR/$binary.tar.gz" 1>&2
+	mkdir -p "$CACHE_DIR"
+	if [ ! -f $CACHE_DIR/$binary.tar.gz ]; then
 		echo "         Downloading Binary for the first time" 1>&2
 		curl -sSL --get --retry 3 $1 \
 			-o $output && gunzip -t $output &>/dev/null || ( cat $output 1>&2 && exit 1 )
-		mv $output $cache_dir/$binary.tar.gz
+		mv $output $CACHE_DIR/$binary.tar.gz
 	fi
-	cat $cache_dir/$binary.tar.gz
+	cat $CACHE_DIR/$binary.tar.gz
 }
 
 buildcurl() {
-	local cache_dir="$cache_dir/buildcurl"
+	local CACHE_DIR="$CACHE_DIR/buildcurl"
 	local binary=$(basename $1 .tar.gz)
 	local recipe=${binary/-*/}
 	local version=${binary/*-/}
@@ -25,13 +25,13 @@ buildcurl() {
 	local output=$(mktemp)
 
 	echo "Buildcurl ($1) for binary:$binary output:$output to out_prefix:$out_prefix" 1>&2
-	mkdir -p "$cache_dir"
-	if [ ! -f $cache_dir/$binary.tar.gz ]; then
+	mkdir -p "$CACHE_DIR"
+	if [ ! -f $CACHE_DIR/$binary.tar.gz ]; then
 		echo "         Compiling $binary for the first time will take a few minutes..." 1>&2
 		curl -sSL --get --retry 3 ${BUILDCURL_URL:="buildcurl.com"} \
 			-d recipe=$recipe -d version=$version -d target=$TARGET -d prefix=$out_prefix \
 			-o $output && gunzip -t $output &>/dev/null || ( cat $output 1>&2 && exit 1 )
-		mv $output $cache_dir/$binary.tar.gz
+		mv $output $CACHE_DIR/$binary.tar.gz
 	fi
-	cat $cache_dir/$binary.tar.gz
+	cat $CACHE_DIR/$binary.tar.gz
 }
