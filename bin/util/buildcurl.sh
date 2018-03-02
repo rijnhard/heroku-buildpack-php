e1525086e08cead68867cbf1bd7d080ed3763fdc
Rijnhard Hessel <rijnhard@teleforge.co.za>
2018-03-02 12:19:39 +0200

    turned off download progress output for composer reverted blackfire and new_relic changes

--- bin/util/buildcurl.sh
+++ bin/util/buildcurl.sh
@@ -5,10 +5,10 @@ cachecurl() {
 	local version=${binary/*-/}
 	local output=$(mktemp)
 
-	notice_inline "Cachecurl ($1) for binary:$binary in output:$output but to $cache_dir/$binary.tar.gz" 1>&2
+	notice_inline "Cachecurl ($1) for binary:$binary in output:$output but to $cache_dir/$binary.tar.gz"
 	mkdir -p "$cache_dir"
 	if [ ! -f $cache_dir/$binary.tar.gz ]; then
-		notice_inline "         Downloading Binary for the first time" 1>&2
+		notice_inline "         Downloading Binary for the first time"
 		curl -sSL --get --retry 3 $1 \
 			-o $output && gunzip -t $output &>/dev/null || ( cat $output 1>&2 && exit 1 )
 		mv $output $cache_dir/$binary.tar.gz
@@ -24,10 +24,10 @@ buildcurl() {
 	local out_prefix=${2:-"/app/.heroku/php"}
 	local output=$(mktemp)
 
-	notice_inline "Buildcurl ($1) for binary:$binary output:$output to out_prefix:$out_prefix" 1>&2
+	notice_inline "Buildcurl ($1) for binary:$binary output:$output to out_prefix:$out_prefix"
 	mkdir -p "$cache_dir"
 	if [ ! -f $cache_dir/$binary.tar.gz ]; then
-		notice_inline "         Compiling $binary for the first time will take a few minutes..." 1>&2
+		notice_inline "         Compiling $binary for the first time will take a few minutes..."
 		curl -sSL --get --retry 3 ${BUILDCURL_URL:="buildcurl.com"} \
 			--data-urlencode recipe=$recipe \
 			--data-urlencode version=$version \
